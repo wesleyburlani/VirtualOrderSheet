f22d5ad2648e7d34f27b925a03f959631ae5e2a5
vini2reis <vinicius.reis@estudante.uffs.edu.br>
2019-09-16 15:43:57 -0300

    Listagem de Produtos

---
+++ client/src/Pages/Product/Index.js
@@ -0,0 +1,81 @@
+import React from 'react'
+
+import { Table, PageHeader, Icon , Divider, Button } from 'antd'
+
+export default () => {
+
+  const dataSource = [
+    {
+        referenceCode: '1',
+        name: 'Bebida A',
+        price: 8.00,
+        description: 'SsS',
+        stock: 10,
+    },
+    {
+        referenceCode: '2',
+        name: 'Bebida B',
+        price: 10.00,
+        description: 'ACF',
+        stock: 15,
+      },
+  ]
+  
+  const columns = [
+    {
+      title: 'Bebida',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Preço',
+      dataIndex: 'price',
+    },
+    {
+      title: 'Descrição',
+      dataIndex: 'description',
+    },
+    {
+      title: 'Estoque',
+      dataIndex: 'stock',  
+    },
+    {
+      title: 'Ações',
+      key: 'actions',
+      fixed: 'right',
+      width: 100,
+      render: () => (
+        <div>
+          <a>
+            <Icon type="edit" />
+          </a>
+          <Divider type="vertical"/>
+          <a style={{ color: 'red' }}>
+            <Icon type="delete" />
+          </a>
+        </div>
+      )
+    },
+  ]
+
+
+  
+  
+
+  return (
+    <div>
+      <PageHeader
+        onBack={() => null}
+        title="Lista de Produtos"
+        extra={[
+          <Button type="primary" icon="plus" key="new">
+            Novo Produto
+          </Button>
+        ]}
+      />
+      <Table
+        dataSource={dataSource}
+        columns={columns}
+      />
+    </div>
+  )
+} 
